030603e527b743fafac52a7b2ab594688dd36724
zia <zia.ulhafidz@nostratech.com>
2025-02-12 23:55:38 +0700

    snake and ladder game

---
+++ App.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int[] cells = new int[100];
+        int totalSnakes = sc.nextInt();
+        for (int i = 0; i < totalSnakes; i++)
+            cells[sc.nextInt()] = sc.nextInt();
+
+        int totalLadders = sc.nextInt();
+        for (int i = 0; i < totalLadders; i++)
+            cells[sc.nextInt()] = sc.nextInt();
+
+        int totalPlayers = sc.nextInt();
+        Player[] players = new Player[totalPlayers];
+        for (int i = 0; i < totalPlayers; i++)
+            players[i] = new Player(sc.next());
+
+        Game game = new Game(cells, totalPlayers, players);
+        game.startGame();
+
+        sc.close();
+    }
+}
---
+++ Dice.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+
+public class Dice {
+    public static int roll() {
+        return new Random().nextInt(6) + 1;
+    }
+}
